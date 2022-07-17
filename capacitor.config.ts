85ec03e273ddaae84998144abccd6e3c38c6b4bc
jonz94 <jody16888@gmail.com>
2022-07-17 13:52:51 +0800

    initial commit, generated by `npx @ionic/cli start`

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-tailwind-starter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
