a8359659a0c0689bb82496218ce2e05bae30acfa
jonz94 <jody16888@gmail.com>
2022-10-02 20:46:33 +0800

    chore: remove duplicated routing

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,19 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 import { Tab3Page } from './tab3.page';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }]),
-    Tab3PageRoutingModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, Tab3PageRoutingModule],
   declarations: [Tab3Page],
 })
 export class Tab3PageModule {}
